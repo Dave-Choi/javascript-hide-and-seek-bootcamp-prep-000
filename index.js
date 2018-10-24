933bd80f407f348c60c312d6f35fd67ea346833d
David Choi <daaave@gmail.com>
2018-10-24 00:04:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n){
+  let rankNodes = document.querySelectorAll(".ranked-list li");
+  for(let i=0; i<rankNodes.length; i++){
+    let item = rankNodes[i];
+    let value = parseInt(item.innerHTML);
+    item.innerHTML = value + n;
+  }
+}
+
+function deepestChild(){
+  let root = document.getElementById("grand-node");
+  let deepest = root;
+  let depth = 0;
+  
+  while(deepest.children){
+    deepest = deepest.children[0];
+    depth++;
+  }
+  return depth;
+  return deepest;
+}
